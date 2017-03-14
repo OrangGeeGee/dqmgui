728e8d907716c87619f10880a6d3b9bb4238fc4e
Antanas Sinica <antanas.sinica@gmail.com>
2017-03-14 15:15:18 +0100

    Integration tests visDQMUpload used as primary upload method (if found on path)

--- test/integration/base.py
+++ test/integration/base.py
@@ -3,12 +3,14 @@ import ConfigParser
 import hashlib
 import json
 import os
+import subprocess
 import re
-import requests
 import time
 import shutil
 import tempfile
+import urllib2
 from stat import ST_SIZE
+from sys import stdout
 
 import rootgen
 
@@ -34,6 +36,20 @@ class BaseIntegrationTest(unittest.TestCase):
 
     def upload(self, filename, path):
 
+        try:
+            subprocess.check_call(["visDQMUpload", self.base_url, path])
+        except (OSError, subprocess.CalledProcessError) as e:
+            print 'visDQMUpload script invokation failed'
+            print e
+            try:
+                self.upload_requests(filename, path)
+            except ImportError as e:
+                print 'Either visDQMUpload must be available on path or requests library must be installed manually.'
+                raise e
+
+    def upload_requests(self, filename, path):
+        import requests
+
         url = self.base_url + 'data/put'
         files = {'file': (filename, open(path, 'rb'))}
         data = {'size': str(os.stat(path)[ST_SIZE]),
@@ -53,32 +69,32 @@ class BaseIntegrationTest(unittest.TestCase):
         return filename, run, dataset
 
     def session(self):
-        create_session_response = requests.get(self.base_url)
-        self.assertTrue(create_session_response.ok,
-                        'Request failed. Status code received ' + str(create_session_response.status_code))
+        create_session_response = urllib2.urlopen(self.base_url)
+        create_session_content = create_session_response.read()
+        self.assertEquals(create_session_response.getcode(), 200, 'Request failed. Status code received not 200')
 
-        session = re.search('/dqm/dev/session/([\w\d]+)', str(create_session_response.content))
+        session = re.search('/dqm/dev/session/([\w\d]+)', create_session_content)
         return session.group(1)
 
     def chooseSample(self, session):
         choose_sample_url = '%ssession/%s/chooseSample?vary=run;order=dataset' % (self.base_url, session)
-        response = requests.get(choose_sample_url)
-        self.assertTrue(response.ok,
-                        'Request failed. Status code received ' + str(response.status_code))
-        content = response.content
+        response = urllib2.urlopen(choose_sample_url)
+        content = response.read()
+        self.assertEquals(response.getcode(), 200, 'Request failed. Status code re1ceived not 200')
         content = re.sub('\)$', '', re.sub('^\(', '', content)).replace('\'', '"')
         json_content = json.loads(content)
         return json_content
 
     def uploadWatch(self, dataset):
-        print 'Upload watch started for dataset %s. Checking every %s seconds' % (dataset, self.upload_watch_interval),
-
+        stdout.write('Upload watch started for dataset %s. Checking every %s seconds' % (dataset, self.upload_watch_interval))
+        stdout.flush()
         session = self.session()
         start_time = time.time()
         i = 0
         while i < self.upload_watch_retries:
             time.sleep(self.upload_watch_interval)
-            print '.',
+            stdout.write('.')
+            stdout.flush()
             choose_sample_json = self.chooseSample(session)
             for types in choose_sample_json[1]['items']:
                 for item in types['items']:
